92f7d5815938d2f2995ee6a24d1a8de3b56e913a
John <gerakl00@gmail.com>
2019-02-11 14:59:12 +0500

    - оптимизация для поисковиков - дополнение страницы с контактами - добавлена sitemap - добавлена ссылка на группу ВК

--- mailer.php
+++ mailer.php
@@ -5,11 +5,21 @@
  * Date: 07/02/2019
  * Time: 17:35
  */
-echo 'hello';
-$message = 'Hello^ i am Jonny';
-$test =
-    mail("evg.kuryatov@gmail.com", "the subject", $message,
+
+$name = htmlspecialchars($_POST['name']);
+$number = htmlspecialchars($_POST['number']);
+
+$subject = "Новое обращение с сайта lorgis74.ru";
+
+$message =
+    "<b>Обращение от:</b> {$name} <br>
+     <b>Номер телефона:</b><br> {$number}";
+
+$mailto = "evg.kuryatov@gmail.com";
+
+$mail =
+    mail($mailto, $subject, $message,
         "From: webmaster@ example.com \r\n"
         ."X-Mailer: PHP/" . phpversion());
-echo $test;
-phpinfo();
+
+var_dump($mail);
