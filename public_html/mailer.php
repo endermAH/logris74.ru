a81bb3e898387be64008d281b3151767de2677cc
John <gerakl00@gmail.com>
2019-02-13 12:43:24 +0500

    correct path

--- public_html/mailer.php
+++ public_html/mailer.php
@@ -1,7 +1,12 @@
 <?php
 
-require_once "vendor/autoload.php";
+require_once "../vendor/autoload.php";
 use PHPMailer\PHPMailer\PHPMailer;
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 
 $name = htmlspecialchars($_POST['name']);
 $number = htmlspecialchars($_POST['number']);
